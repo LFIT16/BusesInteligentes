f548f3379380e5dc1e648c4f9f35e4a7dfb151d0
LFIT16 <ibarratucano@gmail.com>
2026-03-20 16:36:10 -0500

    Validaciones de usuario

--- src/main/java/com/lfit/ms_security/Models/User.java
+++ src/main/java/com/lfit/ms_security/Models/User.java
@@ -1,5 +1,8 @@
 package com.lfit.ms_security.Models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
@@ -11,13 +14,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private String id;
+
+    @NotBlank(message = "El nombre es obligatorio")
     private String name;
+
+    @Email(message = "Debe ser un email válido")
+    @NotBlank
     private String email;
+
+    @NotBlank( message = "La contraseña es obligatoria")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$",
+            message = "La contraseña debe tener mínimo 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial"
+    )
     private String password;
-    public User() {
 
-    }
-
+    public User() {}
 
     public User(String name, String email, String password) {
         this.name = name;
