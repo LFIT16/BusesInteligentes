287e8b8fca68e6f832c050da47f381a3a8abb8ad
Isabela <guerreroisabela243@gmail.com>
2026-04-05 17:07:17 -0500

    fix: autenticación OAuth Google

--- src/main/java/com/lfit/ms_security/Repositories/UserRepository.java
+++ src/main/java/com/lfit/ms_security/Repositories/UserRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Optional;
 
-public interface UserRepository  extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
 
     @Query("{'email': ?0}")
     public User getUserByEmail(String email);
+
     Optional<User> findByGithubUsername(String githubUsername);
+
+    Optional<User> findByGoogleId(String googleId);
+
+    Optional<User> findByMicrosoftId(String microsoftId);
 }
